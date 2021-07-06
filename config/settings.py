e16995342a96dff077b67a26c98d3a730e6177bd
robin <nurulhudarobin@gmail.com>
2021-07-06 15:23:29 +0600

    postgres config for json field

--- config/settings.py
+++ config/settings.py
@@ -92,8 +92,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'HOST': env('DB_HOST'),
+        'PASSWORD': env('DB_PASSWORD'),
     }
 }
 
